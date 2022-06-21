f3ea2071c6029e32f25857a4326fd1915c38e2b0
Samonto Karmaker <samonto0726@gmail.com>
2022-06-21 20:56:28 +0600

    Debug: <p> to <CardText>

--- src/Components/body/DishDetails.js
+++ src/Components/body/DishDetails.js
@@ -15,9 +15,13 @@ const DishDetails = props => {
                         <h2>{props.dish.name}</h2>
                     </CardTitle>
                     <CardText>
-                        <p>{props.dish.description}</p>
-                        <p>Category: {props.dish.category}</p>
-                        <p>tk{props.dish.price}/-</p>
+                        {props.dish.description}
+                    </CardText>
+                    <CardText>
+                        Category: {props.dish.category}
+                    </CardText>
+                    <CardText>
+                        tk{props.dish.price}/-
                     </CardText>
                     <hr/>
                     <LoadComments comments = {props.dish.comments} />
