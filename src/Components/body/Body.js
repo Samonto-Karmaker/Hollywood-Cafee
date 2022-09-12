9d6f3595f804ede06f478af184a50c4ba58c5b49
Samonto Karmaker <samonto0726@gmail.com>
2022-09-12 20:54:11 +0600

    Added react router

--- src/Components/body/Body.js
+++ src/Components/body/Body.js
@@ -3,11 +3,17 @@ import Menu from './Menu';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import { Route, Routes } from 'react-router';
 
 const Body = () => {
     return(
         <div>
-            
+            <Routes>
+                <Route path = "/" exact element = {<Home />}/>
+                <Route path = "/menu" exact element = {<Menu />}/>
+                <Route path = "/about" exact element = {<About />}/>
+                <Route path = "/contact" exact element = {<Contact />}/>
+            </Routes>
         </div>
     )
 }
